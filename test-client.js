e6e4093faad795e563b1d9052beed6d896e579b8
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-17 08:56:49 +0200

    Test the insertMany method

--- test-client.js
+++ test-client.js
@@ -3,6 +3,22 @@ const { FileDB } = require('./index');
 const main = () => {
   // create db
   const db = new FileDB('db');
+
+  const user = db.createDocument('user');
+  user.insertMany([
+    {
+      name: 'Ahmed',
+      age: 20,
+    },
+    {
+      name: 'Hend',
+      age: 22,
+    },
+    {
+      name: 'Jone',
+      age: 50,
+    },
+  ]);
 };
 
 main();
