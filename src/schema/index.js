cea69979b8319b44c94ae09a2a413cbdbdf54120
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-18 22:34:45 +0200

    Run the schema against the data

--- src/schema/index.js
+++ src/schema/index.js
@@ -1,9 +1,19 @@
+const { throwError } = require('../utils/utils');
+
 module.exports = class Schema {
   constructor(schemaFields) {
     this.schema = schemaFields;
   }
 
   validateDataAganistSchema(data) {
-    console.log(data, this.schema);
+    for (let schemaField in this.schema) {
+      const schemaType = typeof this.schema[schemaField]();
+      const dataType = typeof data[schemaField];
+      if (schemaType !== dataType) {
+        throwError(
+          ` Field ${schemaField} expected type of ${schemaType} but get type of ${dataType} `
+        );
+      }
+    }
   }
 };
