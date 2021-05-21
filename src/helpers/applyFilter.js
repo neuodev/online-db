780c2aac2a53c5baa8cfb30efa48b3d099c558e4
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-21 22:19:19 +0200

    Apply sorting for strings

--- src/helpers/applyFilter.js
+++ src/helpers/applyFilter.js
@@ -33,20 +33,38 @@ module.exports.applyFilter = (filters, data) => {
   }
   if (typeof filters.sort !== 'undefined') {
     const fieldsToSortBy = filters.sort.split(' ');
-    let currentSortedField = fieldsToSortBy[0]
-   
+    let currentSortedField = fieldsToSortBy[0];
+
     let currentField = currentSortedField.startsWith('-')
       ? currentSortedField.slice(1)
       : currentSortedField;
-    console.log(currentField);
-    // sort by number
+    // sorting
     if (typeof data[0][currentField] === 'number') {
       data = data.sort((a, b) => {
+        console.log(currentSortedField, currentField);
         if (!currentSortedField.startsWith('-')) {
           return a[currentField] - b[currentField];
         } else {
           return b[currentField] - a[currentField];
         }
+      });
+    }
+    if (typeof data[0][currentField] === 'string') {
+      data = data.sort((a, b) => {
+        console.log(currentSortedField, currentField);
+        if (
+          !currentSortedField.startsWith('-') &&
+          a[currentField] < b[currentField]
+        ) {
+          return -1;
+        }
+        if (
+          a[currentField] > b[currentField] &&
+          currentSortedField.startsWith('-')
+        ) {
+          return 1;
+        }
+        return 0;
       });
     }
     // sort by chars
