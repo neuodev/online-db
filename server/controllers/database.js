615e12bbf408b250250aced8bb947d20db3bbf61
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-23 18:47:23 +0200

    Update the database contorller

--- server/controllers/database.js
+++ server/controllers/database.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 
-//@desc  get all databases and its documents  
+//@desc  get all databases and its documents
 //@route GET api/v1/database
 //@access Public
 module.exports.getDatabases = (req, res, next) => {
   // like -> [firstDatase, secondDatabse]
   let results = {};
-  const databases = fs.readdirSync();
+  const databases = fs.readdirSync('./OnlineDB');
 
   for (let database of databases) {
-    const files = fs.readdirSync(database);
+    const files = fs.readdirSync(`./OnlineDB/${database}`);
     let collections = [];
     for (let fileName of files) {
       collections.push(fileName.split('.')[0]);
