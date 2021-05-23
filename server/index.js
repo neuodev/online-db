60a6efef401010dfb007cd20ead998389befdb83
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-23 19:34:06 +0200

    Add error handler

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const colros = require('colors');
 const databaseRouter = require('./routes/database');
+const errorHandler = require('./middlewares/error');
 
 const startServer = () => {
   const app = express();
@@ -14,7 +15,9 @@ const startServer = () => {
 
   // API Routes 
   app.use('/api/v1', databaseRouter)
-
+  
+  // Error Handling 
+  app.use(errorHandler)
   const PORT = 9000;
   app.listen(PORT, () =>
     console.log(`Visualize you database on port ${PORT} `.bgGreen)
--- server/middlewares/error.js
+++ server/middlewares/error.js
@@ -1,3 +1,9 @@
 const colors = require('colors');
 
-
+const errorHandler = (err, req, res, next) => {
+  console.log(error.message.bgRed);
+  res.status(err.statusCode || 500).json({
+    error: err.message,
+  });
+};
+module.exports = errorHandler;
