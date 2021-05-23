6e6761a7edc2e6250f5386f22604d938dfad22d5
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-23 21:48:39 +0200

    Setup the collection contorller

---
+++ server/controllers/collection.js
@@ -0,0 +1,21 @@
+//@desc   get document details
+//@route  PUT api/v1/collection/:database/:collection
+//@access Public
+module.exports.getDocumentDtails = (req, res, next) => {
+  const { database, collection } = req.params;
+  const databaseLowercase = database.toLowerCase();
+  const collectionLowercase = collection.toLowerCase();
+  // check if the db exist
+  const DB_PATH = `./OnlineDB/${databaseLowercase}`;
+  const isDBExist = fs.existsSync(DB_PATH);
+  if (!isDBExist) return next(new ErrorResponse(`Database not found`, 400));
+
+  // check if the collection exist
+  const COLLECTION_PATH = `./OnlineDB/${databaseLowercase}/${collectionLowercase}.onlinedb.db`;
+  const isDocExist = fs.existsSync(COLLECTION_PATH);
+  if (!isDocExist) return next(new ErrorResponse(`Collection not found`, 400));
+
+
+
+  res.status(201).json({ success: true });
+};
--- server/index.js
+++ server/index.js
@@ -7,17 +7,16 @@ const errorHandler = require('./middlewares/error');
 const startServer = () => {
   const app = express();
 
-  // access from anoter port 
+  // access from anoter port
   app.use(cors());
 
   // To allow write data to the server
   app.use(express.json());
+  // API Routes
+  app.use('/api/v1', databaseRouter);
 
-  // API Routes 
-  app.use('/api/v1', databaseRouter)
-  
-  // Error Handling 
-  app.use(errorHandler)
+  // Error Handling
+  app.use(errorHandler);
   const PORT = 9000;
   app.listen(PORT, () =>
     console.log(`Visualize you database on  http://localhost:${PORT} `.bgGreen)
