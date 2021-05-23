33c2c2ebfce8938f845c50a25b9b16a29e7950ff
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-23 18:54:38 +0200

    Update gitignore file

--- server/controllers/database.js
+++ server/controllers/database.js
@@ -19,3 +19,23 @@ module.exports.getDatabases = (req, res, next) => {
 
   res.status(200).json(results);
 };
+
+//@desc  Create new Database
+//@route POST api/v1/database
+//@access Public
+module.exports.CreateDatabase = (req, res, next) => {
+  // like -> [firstDatase, secondDatabse]
+  let results = {};
+  const databases = fs.readdirSync('./OnlineDB');
+
+  for (let database of databases) {
+    const files = fs.readdirSync(`./OnlineDB/${database}`);
+    let collections = [];
+    for (let fileName of files) {
+      collections.push(fileName.split('.')[0]);
+    }
+    results[database] = collections;
+  }
+
+  res.status(200).json(results);
+};
