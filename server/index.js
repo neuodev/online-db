ae59777a4c53906a224ff6a4ac51514f5c5fe628
Ahmed Ibrahim <ib153507@gmail.com>
2021-05-24 22:35:18 +0200

    Apply validation for the ctx

--- server/index.js
+++ server/index.js
@@ -4,6 +4,10 @@ const colros = require('colors');
 const databaseRouter = require('./routes/database');
 const errorHandler = require('./middlewares/error');
 const collectionRouter = require('./routes/collection');
+const ErrorResponse = require('./utils/ErrorResponse');
+
+const { OnlineDB } = require('../index');
+const Collection = require('../src/classes/Collection');
 
 const startServer = context => {
   const app = express();
@@ -16,6 +20,47 @@ const startServer = context => {
 
   // inject the database info
   app.use((req, res, next) => {
+    // context should be of type array
+    if (!(context instanceof Array))
+      next(
+        new ErrorResponse(
+          `Expected the context to be type "array" but get type"${typeof context}"`
+        )
+      );
+    // every item should be an object with two fields database -> string , documents array
+
+    for (let info of context) {
+      const { database, documents } = info;
+
+      if (typeof database == 'undefined')
+        return next(new ErrorResponse(`Missing the database field`.bgRed, 400));
+
+      if (!(database instanceof OnlineDB))
+        return next(new ErrorResponse(`Invalide database field`.bgRed, 400));
+
+      if (typeof documents === 'undefined')
+        return next(
+          new ErrorResponse(`Missing the documents field`.bgRed, 400)
+        );
+
+      if (!(documents instanceof Array))
+        return next(
+          new ErrorResponse(
+            `document field expected to be an array but get a/an ${typeof documents}`.bgRed,
+            400
+          )
+        );
+      for (let document of documents) {
+        if (!(document instanceof Collection))
+          return next(
+            new ErrorResponse(
+              `"${document}" is not a valid document `.bgRed,
+              400
+            )
+          );
+      }
+    }
+
     req.context = context;
     next();
   });
@@ -23,7 +68,7 @@ const startServer = context => {
   // API Routes
   app.use('/api/v1', databaseRouter);
   app.use('/api/v1', collectionRouter);
-  
+
   // Error Handling
   app.use(errorHandler);
   const PORT = 9000;
